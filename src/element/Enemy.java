ce5da482f21008df2aee136484a4946af46da379
ajnOZDd <107546913+ajnOZDd@users.noreply.github.com>
2022-11-28 14:49:59 +0100

    comit

--- src/element/Enemy.java
+++ src/element/Enemy.java
@@ -1,5 +1,11 @@
 package element;
 
 public class Enemy {
-    
+    String nomEnemy ;
+    int pointVie ;
+    double puissance  ;
+    int level ;
+    double vitesse ;
+    String [] nameArm = {"missile" ,"pisolet" , "epee"};
+
 }
